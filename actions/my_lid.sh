4a226e531cdd102defa4af14a22607e03dcea806
Lars Moellendorf <lars@moellendorf.eu>
2012-04-18 23:49:32 +0200

    inline function

--- actions/my_lid.sh
+++ actions/my_lid.sh
@@ -46,7 +46,18 @@ getDBusSessionAddress () {
 xsu () {
     log "su $XUSER"
     su $XUSER
-    getDBusSessionAddress
+    log "I am: $(whoami)"
+    log "home is: $HOME"
+    log "DBUS_SESSION_BUS_ADDRESS is: $DBUS_SESSION_BUS_ADDRESS"
+    if [[ -z "$DBUS_SESSION_BUS_ADDRESS" ]]; then
+        log "get dbus session address"
+        # Looks like we are outside X
+        dbus_file=$(ls $HOME/.dbus/session-bus/ -t | head -1)
+        # Get the latest file in session-bus directory
+        . "$HOME/.dbus/session-bus/$dbus_file" && export DBUS_SESSION_BUS_ADDRESS
+        # and export a variable from it
+    fi
+    log "dbus session address is: $DBUS_SESSION_BUS_ADDRESS"
     log "DISPLAY=$DISPLAY \"$@\""
     ERROR=$( { DISPLAY=$DISPLAY "$@"; } 2>&1 )
     log "$ERROR"
