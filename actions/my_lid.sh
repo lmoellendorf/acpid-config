a5d7c951d2525e5fced81729de5c0f944732d07d
Lars Moellendorf <lars@moellendorf.eu>
2012-11-02 02:37:50 +0100

    added copyright and license info

--- actions/my_lid.sh
+++ actions/my_lid.sh
@@ -1,5 +1,24 @@
 #! /bin/sh
-# lid button pressed/released event handler
+# Lid button pressed/released event handler
+#
+# Copyright (C) 2012 Lars Moellendorf <lars[at]moellendorf[dot]eu>
+# Distributed under the terms of the GNU General Public License v2
+#
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 # default display on current host
 DISPLAY=":0.0"
